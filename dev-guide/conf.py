186549da2378acdf88a856ec696adfc16d7ea30d
Cat Lookabaugh <cat.lookabaugh@rackspace.com>
2015-09-25 13:36:49 -0500

    added deconst build thingy

--- dev-guide/conf.py
+++ dev-guide/conf.py
@@ -45,6 +45,10 @@ source_suffix = '.rst'
 # The master toctree document.
 master_doc = 'index'
 
+# The builder to use when running via the deconst preparer
+# builder = 'deconst-serial'
+builder = 'deconst-single'
+
 # General information about the project.
 project = u'Rackspace Developer Portal'
 copyright = u'2015, Rackspace'
